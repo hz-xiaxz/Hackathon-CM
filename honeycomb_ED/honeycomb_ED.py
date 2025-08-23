4ab7ebe5090f69f286a2f69a81bde82dbd52f13a
hzxiaxz <1806656034@qq.com>
2025-08-23 19:23:03 +0800

    update

--- honeycomb_ED/honeycomb_ED.py
+++ honeycomb_ED/honeycomb_ED.py
@@ -23,9 +23,9 @@ import matplotlib.pyplot as plt  # plotting library
 #
 ###### create honeycomb lattice
 # lattice graph parameters
-m = 1  # number of rows of hexagons in the lattice
+m = 2  # number of rows of hexagons in the lattice
 n = 2  # number of columns of hexagons in the lattice
-isPBC = False  # if True, use periodic boundary conditions
+isPBC = True  # if True, use periodic boundary conditions
 #
 ### build graph using networkx
 hex_graph = nx.generators.lattice.hexagonal_lattice_graph(m, n, periodic=isPBC)
@@ -34,12 +34,14 @@ hex_graph = nx.convert_node_labels_to_integers(hex_graph)
 # set number of lattice sites
 N = hex_graph.number_of_nodes()
 print("constructed hexagonal lattice with {0:d} sites.\n".format(N))
+nx.draw(hex_graph)
+plt.savefig("honeycomb_lattice.png")  # save lattice figure
 
 ###### model parameters
 #
 N_f = m * n * 2 // 2  # number of spinless fermions
 t = 1.0  # tunnelling matrix element
-U = 4.0  # nearest-neighbor interaction strength
+U = 0.0  # nearest-neighbor interaction strength
 #
 ##### set up spinless fermion Hamiltonian with quspin #####
 #
@@ -50,7 +52,8 @@ print("Hilbert space size: {0:d}.\n".format(basis.Ns))
 #
 # define site-coupling lists
 # hopping term: -t * (c_i^dagger c_j + h.c.)
-tunnelling = [[-t, i, j] for i in range(N) for j in hex_graph.adj[i]]
+tunneling = [[-t, i, j] for i in range(N) for j in hex_graph.adj[i]]
+print(len(hex_graph.edges()))
 # nearest-neighbor interaction term: U * n_i * n_j
 # hex_graph.edges() provides all unique nearest-neighbor pairs (i,j)
 interactions = [[U, i, j] for i, j in hex_graph.edges()]
@@ -58,13 +61,13 @@ interactions = [[U, i, j] for i, j in hex_graph.edges()]
 # define operator strings for Hamiltonian
 # "+-" corresponds to hopping c_i^dagger c_j
 # "nn" corresponds to interaction n_i * n_j
-static = [["+-", tunnelling], ["nn", interactions]]
+static = [["+-", tunneling], ["nn", interactions]]
 dynamic = []
 #
 ### construct Hamiltonian
-H = hamiltonian(static, dynamic, basis=basis, dtype=np.float64)
+H = hamiltonian(static, dynamic, basis=basi
+                s, dtype=np.float64)
 #
 # compute eigensystem
 E, V = H.eigsh(k=1, which="SA", maxiter=1e4)
 print(f"\nlowest energies: {E}")
-
